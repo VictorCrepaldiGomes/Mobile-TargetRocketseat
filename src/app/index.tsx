99389195108a7b506b98260fedf17504951583be
Victor Crepaldi <victorcrepaldigomes@gmail.com>
2025-11-18 15:34:08 -0300

    feat: update dependencies in Podfile.lock and package-lock.json; refactor HomeHeader component and adjust tsconfig.json settings

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,7 @@
 import { View } from "react-native";
 
 export default function Index() {
-  return <View style={{ flex: 1 }}></View>;
+  return <View style={{ flex: 1 }}>
+    
+  </View>;
 }
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,7 +1,5 @@
 export default function HomeHeader() {
     return (
-        <div>
-            
-        </div>
+        null
     )
 }
